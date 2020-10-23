31ec560c1648a6b0fca91015b224d9463396a471
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-23 09:58:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(name) {
+    let message = [];
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
+
+function countDown(n) {
+    while (n > -1 )
+    console.log(n--);
+}
