c06dc6b39b112cf54209a7584f2cad0bc90f9f1f
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-10-27 10:31:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const friends = ["Lisa", "Kaitlin", "Jan"]
+ 
+function writeCards(friends, surprise) {
+    let thankYouCards = []
+    for ( let i = 0; i < friends.length; i++ ) {
+      thankYouCards.push( `Thank you, ${friends[i]}, for the wonderful ${surprise} gift!` )
+    }
+    return thankYouCards
+  }
+
+
+function countDown( countDown ) {
+    while ( countDown > 0 ) {
+      console.log( countDown );
+      countDown -= 1;
+    }
+    console.log( countDown );
+  }
