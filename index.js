3969439734a4e0525ea8ebe3146d58a1f7f7a6bb
Your Username <you@example.com>
2020-10-29 15:34:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+function writeCards(arr, eventName){
+    let newArr = new Array();
+    for(let i = 0; i<arr.length; i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArr
+}
+
+let num = 0;
+function countDown(num) {
+    while(num!=0) {
+        console.log(num);
+        num = num-1;
+    }
+    console.log(num);
+}
