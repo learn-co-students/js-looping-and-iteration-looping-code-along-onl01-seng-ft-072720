9f085c596f42c006d70dcc23b87aa606b370f85c
HuffDaddy0 <ty.huff24@gmail.com>
2020-10-29 10:35:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, day){
+    let newArray = []
+    for(let i=0; i<array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${day} gift!`)
+    }
+    return newArray
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n)
+        n--
+    }
+}
