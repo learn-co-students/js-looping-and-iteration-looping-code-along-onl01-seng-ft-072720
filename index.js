555c7fb37a6a2627168874dddfae8acf6f5fb4b8
VirgilCantu <cantu.sampsom@gmail.com>
2020-11-03 23:46:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let greetings = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        greetings.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return greetings;
+}
+
+function countDown(integer) {
+    let countdown = integer
+    while (countdown > 0) {
+        console.log(countdown)
+        countdown--
+    }
+    console.log(countdown)
+}
